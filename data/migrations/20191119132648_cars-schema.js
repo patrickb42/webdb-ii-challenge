fc9e61735375eedc12d5fc947d512f2639cb07e4
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-11-19 15:06:46 -0500

    added seeds

--- data/migrations/20191119132648_cars-schema.js
+++ data/migrations/20191119132648_cars-schema.js
@@ -1,15 +1,11 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('cars', (tbl) => {
-    tbl.increments();
-    tbl.string('vin', 17).unique().notNullable();
-    tbl.string('make', 128).notNullable();
-    tbl.string('model', 128).notNullable();
-    tbl.decimal('mileage', 1).notNullable();
-    tbl.string('transmissionType', 128);
-    tbl.string('titleStatus', 128);
-  });
-};
+exports.up = (knex) => knex.schema.createTable('cars', (tbl) => {
+  tbl.increments();
+  tbl.string('vin', 17).unique().notNullable();
+  tbl.string('make', 128).notNullable();
+  tbl.string('model', 128).notNullable();
+  tbl.decimal('mileage', 1).notNullable();
+  tbl.string('transmissionType', 128);
+  tbl.string('titleStatus', 128);
+});
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('cars');
-};
+exports.down = (knex) => knex.schema.dropTableIfExists('cars');
